3a58814abddac1eb8952fad925f7fcab7e610ea0
ralf <malfurion@gmx.de>
2021-03-05 22:10:50 +0100

    mod settings fixed

--- Source/NeatFreakOrSlobMod.cs
+++ Source/NeatFreakOrSlobMod.cs
@@ -14,7 +14,7 @@ namespace NeatFreakOrSlob
         {
             settings = GetSettings<NeatFreakOrSlobSettings>();
 
-            Harmony.DEBUG = true;
+            //Harmony.DEBUG = true;
             new Harmony("m1st4x.NeatFreakOrSlob").PatchAll();
         }
 
@@ -24,24 +24,25 @@ namespace NeatFreakOrSlob
         {
             Listing_Standard listing_Standard = new Listing_Standard();
             listing_Standard.Begin(inRect);
-            listing_Standard.Label("vanilla filth pickup chance is 5% (0.05), filth drop chance is 10% (0.1)");
-            listing_Standard.TextFieldNumericLabeled("filth pickup chance ", ref settings.globalPickupChance, ref buf1);
-            listing_Standard.TextFieldNumericLabeled("filth drop chance ", ref settings.globalDropChance, ref buf2);
-            listing_Standard.Gap();
-            listing_Standard.TextFieldNumericLabeled("neat freak filth pickup chance ", ref settings.neatPickupChance, ref buf3);
-            listing_Standard.TextFieldNumericLabeled("neat freak filth drop chance ", ref settings.neatDropChance, ref buf4);
-            listing_Standard.Gap();
-            listing_Standard.TextFieldNumericLabeled("slob filth pickup chance ", ref settings.slobPickupChance, ref buf5);
-            listing_Standard.TextFieldNumericLabeled("slob filth drop chance ", ref settings.slobDropChance, ref buf6);
-            listing_Standard.Gap();
-            listing_Standard.TextFieldNumericLabeled("neat freak vs slob relation offset ", ref settings.neatVsSlobOffset, ref buf7);
+            listing_Standard.Label("vanilla filth pickup chance is 10% (0.1), filth drop chance is 5% (0.05), changes apply to all pawns on the map!");
+            listing_Standard.TextFieldNumericLabeled("filth pickup chance ", ref settings.globalPickupChance, ref buf1, 0, 1);
+            listing_Standard.TextFieldNumericLabeled("filth drop chance ", ref settings.globalDropChance, ref buf2, 0, 1);
             listing_Standard.GapLine();
-            listing_Standard.Label("mood (de)buffs based on room cleanliness:");
-            listing_Standard.TextFieldNumericLabeled("neat freak buff threshold ", ref settings.neatBuffThreshold, ref buf8);
-            listing_Standard.TextFieldNumericLabeled("neat freak debuff threshold ", ref settings.neatBuffThreshold, ref buf9);
+            listing_Standard.Label("neat freak and slob chances:");
+            listing_Standard.TextFieldNumericLabeled("neat freak filth pickup chance ", ref settings.neatPickupChance, ref buf3, 0, 1);
+            listing_Standard.TextFieldNumericLabeled("neat freak filth drop chance ", ref settings.neatDropChance, ref buf4, 0, 1);
             listing_Standard.Gap();
-            listing_Standard.TextFieldNumericLabeled("slob buff threshold ", ref settings.slobBuffThreshold, ref buf10);
-            listing_Standard.TextFieldNumericLabeled("slob debuff threshold ", ref settings.slobDebuffThreshold, ref buf11);
+            listing_Standard.TextFieldNumericLabeled("slob filth pickup chance ", ref settings.slobPickupChance, ref buf5, 0, 1);
+            listing_Standard.TextFieldNumericLabeled("slob filth drop chance ", ref settings.slobDropChance, ref buf6, 0, 1);
+            listing_Standard.Gap();
+            listing_Standard.TextFieldNumericLabeled("neat freak vs. slob relation offset ", ref settings.neatVsSlobOffset, ref buf7, -100, 100);
+            listing_Standard.GapLine();
+            listing_Standard.Label("neat freak and slob mood (de)buffs based on room cleanliness:");
+            listing_Standard.TextFieldNumericLabeled("neat freak buff threshold ", ref settings.neatBuffThreshold, ref buf8, -10, 10);
+            listing_Standard.TextFieldNumericLabeled("neat freak debuff threshold ", ref settings.neatDebuffThreshold, ref buf9, -10, 10);
+            listing_Standard.Gap();
+            listing_Standard.TextFieldNumericLabeled("slob buff threshold ", ref settings.slobBuffThreshold, ref buf10, -10, 10);
+            listing_Standard.TextFieldNumericLabeled("slob debuff threshold ", ref settings.slobDebuffThreshold, ref buf11, -10, 10);
             listing_Standard.End();
 
             settings.Write();
--- Source/NeatFreakOrSlobSettings.cs
+++ Source/NeatFreakOrSlobSettings.cs
@@ -4,32 +4,32 @@ namespace NeatFreakOrSlob
 {
     class NeatFreakOrSlobSettings : ModSettings
     {
-        public float globalPickupChance = 0.1f; // 10%
-        public float globalDropChance = 0.05f;  // 5%
-        public float neatPickupChance = 0.05f;  // 5%
-        public float neatDropChance = 0.01f;    // 1%
-        public float slobPickupChance = 0.2f;   // 20%
-        public float slobDropChance = 0.1f;     // 10%
-        public float neatBuffThreshold = 2f;    // x > 2 : buff        
-        public float neatDebuffThreshold = 0f;  // x < 0 : debuff
-        public float slobBuffThreshold = -2f;   // -2 < x < 0 : buff        
-        public float slobDebuffThreshold = 2f;  // x > 2 : debuff
-        public int neatVsSlobOffset = -20;      // relation malus
+        public float globalPickupChance = .1f; // 10%
+        public float globalDropChance = .05f;  // 5%
+        public float neatPickupChance = .05f;  // 5%
+        public float neatDropChance = .01f;    // 1%
+        public float slobPickupChance = .4f;   // 40%
+        public float slobDropChance = .2f;     // 20%
+        public float neatBuffThreshold = 2;    // x > 2 : buff
+        public float neatDebuffThreshold = 0;  // x < 0 : debuff
+        public float slobBuffThreshold = -2;   // -2 < x < 0 : buff
+        public float slobDebuffThreshold = 2;  // x > 2 : debuff
+        public int neatVsSlobOffset = -20;     // relation malus
 
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref this.globalPickupChance, "globalPickupChance", 0.1f);
-            Scribe_Values.Look(ref this.globalDropChance, "globalDropChance", 0.05f);
-            Scribe_Values.Look(ref this.neatPickupChance, "neatPickupChance", 0.05f);
-            Scribe_Values.Look(ref this.neatDropChance, "neatDropChance", 0.01f);
-            Scribe_Values.Look(ref this.slobPickupChance, "slobPickupChance", 0.2f);
-            Scribe_Values.Look(ref this.slobDropChance, "slobDropChance", 0.1f);
-            Scribe_Values.Look(ref this.neatBuffThreshold, "neatBuffThreshold", 2f);
-            Scribe_Values.Look(ref this.neatDebuffThreshold, "neatDebuffThreshold", 0f);
-            Scribe_Values.Look(ref this.slobBuffThreshold, "slobBuffThreshold", -2f);
-            Scribe_Values.Look(ref this.slobDebuffThreshold, "slobDebuffThreshold", 2f);
-            Scribe_Values.Look(ref this.neatVsSlobOffset, "neatVsSlobOffset");
+            Scribe_Values.Look(ref this.globalPickupChance, "globalPickupChance", .1f);
+            Scribe_Values.Look(ref this.globalDropChance, "globalDropChance", .05f);
+            Scribe_Values.Look(ref this.neatPickupChance, "neatPickupChance", .05f);
+            Scribe_Values.Look(ref this.neatDropChance, "neatDropChance", .01f);
+            Scribe_Values.Look(ref this.slobPickupChance, "slobPickupChance", .4f);
+            Scribe_Values.Look(ref this.slobDropChance, "slobDropChance", .2f);
+            Scribe_Values.Look(ref this.neatBuffThreshold, "neatBuffThreshold", 2);
+            Scribe_Values.Look(ref this.neatDebuffThreshold, "neatDebuffThreshold", 0);
+            Scribe_Values.Look(ref this.slobBuffThreshold, "slobBuffThreshold", -2);
+            Scribe_Values.Look(ref this.slobDebuffThreshold, "slobDebuffThreshold", 2);
+            Scribe_Values.Look(ref this.neatVsSlobOffset, "neatVsSlobOffset", -20);
         }
     }
 }
